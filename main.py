9e00df7bfbf0e13f047d42ab37520459aaafbcee
senji <100248680+senjidev@users.noreply.github.com>
2022-04-05 19:16:31 -0400

    added help command, bio command, and fixed spelling errors in post board and help command ctx.send

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import asyncio as a
 
 """VARIABLES"""
-token_id = open('./token_/token.txt', 'r').read()
+token_id = open('../KBtoken/token.txt', 'r').read()
 client = commands.Bot(command_prefix='/')
 clientINS = discord.Client()
 
@@ -33,5 +33,28 @@ async def p_board(ctx):
 async def rm(ctx, limit: int):
         await ctx.channel.purge(limit=limit)
         await ctx.message.delete()
+
+"""HELP COMMAND"""
+@client.command(name='hlp')
+@commands.has_permissions(administrator=True)
+async def help(ctx):
+    await ctx.send(f'Available Commands:\n\t(Post Board: "/pb" ***Displays The Post Board for events and activities***)\n\t(Bio: "/bio" ***Displays a brief description of KendoBot***)\n')
+
+
+
+"""API INTEGRATIONS"""
+
+
+
+"""BOT DESCRIPTION"""
+@client.command(name="bio")
+async def tell_me_about_yourself(ctx):
+    text = f'My name is {client.user}!\n I was built by senji. At present I have limited features(find out more by typing "/hlp")\n :)'
+    await ctx.send(text)
+
+
+
+
+
 
 client.run(token_id)
